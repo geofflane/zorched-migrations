196279b3d28b2f7a9440f759cabc2d16cc13136f
Geoff Lane <geoff@zorched.net>
2009-05-29 15:13:41 -0500

    Add LGPL license info
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- src/Zorched.Migrations.Core/DriverLoader.cs
+++ src/Zorched.Migrations.Core/DriverLoader.cs
@@ -1,5 +1,21 @@
+// Copyright 2008, 2009 Geoff Lane <geoff@zorched.net>
+// 
+// This file is part of Zorched Migrations a SQL Schema Migration framework for .NET.
+// 
+// Zorched Migrations is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Zorched Migrations is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
-using System.Data;
 using System.Data.Common;
 using System.Reflection;
 using Zorched.Migrations.Framework;
@@ -37,7 +53,7 @@ namespace Zorched.Migrations.Core
             return (IDriver) constructor.Invoke(new object[] {new DbParams(connection), logger});
         }
 
-        public DbConnection GetConnection(Type driverType, string connectionString)
+        private static DbConnection GetConnection(Type driverType, string connectionString)
         {
             var provider = DriverAttribute.GetProvider(driverType);
             var factory = DbProviderFactories.GetFactory(provider);
